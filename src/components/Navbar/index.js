7eeab6f2ec64c6441931dd57cdea4f3f94d92cc4
jcrommar <contact@romuwebs.com>
2020-12-25 22:54:21 -0600

    components folder and files

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
+import {
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements';
+
+const Navbar = ({ toggle }) => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to="/">Ebenezer Dallas | Gerizim</NavLogo>
+                    <MobileIcon onClick={toggle}>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Oracion</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services">Contacto</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="signup">Informacion</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/signin">En Vivo</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SideBtnWrap,
+    SidebarRoute
+} from './SidebarElements';
+
+const Sidebar = ({ isOpen, toggle }) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon />
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about" onClick={toggle}>
+                        Home
+                    </SidebarLink>
+                    <SidebarLink to="discover" onClick={toggle}>
+                        Oracion
+                    </SidebarLink>
+                    <SidebarLink to="services" onClick={toggle}>
+                        Contacto
+                    </SidebarLink>
+                    <SidebarLink to="signup" onClick={toggle}>
+                        Informacion
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">En Vivo</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar;
