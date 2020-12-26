a4c34bac144bffebc4c839313285949f8ad3e7f5
jcrommar <contact@romuwebs.com>
2020-12-26 00:38:33 -0600

    also, fixed typos and errors

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -25,7 +25,7 @@ export const CloseIcon = styled(FaTimes)`
 export const Icon = styled.div`
     position: absolute;
     top: 1.2rem;
-    right: 1.5 rem;
+    right: 1.5rem;
     background: transparent;
     font-size: 2rem;
     cursor: pointer;
