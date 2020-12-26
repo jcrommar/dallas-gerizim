cf4baa68f7d4c968aef547885f9b82c29b3d5664
jcrommar <contact@romuwebs.com>
2020-12-26 03:12:28 -0600

    new section for site

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Ministerios Ebenezer',
+    headline: 'Dallas | Gerizim',
+    description: 'Mira nuestros servicios en vivo',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/undraw_navigator_a479.svg'),
+    alt: 'Ministerio',
+    dark: true,
+    primary: true,
+    darkText: false
+};
