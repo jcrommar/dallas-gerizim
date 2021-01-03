d9a9bd7c2cfa0efd355193b62fe98583bca81ac3
jcrommar <contact@romuwebs.com>
2021-01-03 00:45:49 -0600

    changed color to topline to go with palette

--- src/components/InfoSection/InfoElement.js
+++ src/components/InfoSection/InfoElement.js
@@ -53,7 +53,7 @@ export const TextWrapper = styled.div`
 `;
 
 export const TopLine = styled.p`
-    color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#213159')};
+    color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#fbc433')};
     font-size: 16px;
     line-height: 16px;
     font-weight: 700;
