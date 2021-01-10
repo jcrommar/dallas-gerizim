24b1aa2e2532abad735c468d9ac7995a71d6aae7
jcrommar <contact@romuwebs.com>
2021-01-10 16:58:59 -0600

    made changes to the infoSection

--- src/components/InfoSection/InfoElement.js
+++ src/components/InfoSection/InfoElement.js
@@ -100,7 +100,7 @@ export const Heading2 = styled.h1`
 export const Subtitle = styled.p`
     max-width: 440px;
     margin-bottom: 35px;
-    font-size: 18px;
+    font-size: 20px;
     line-height: 24px;
     color: ${({darkText}) => (darkText ? '#010606' : '#fff')};
 `;
@@ -118,6 +118,7 @@ export const ImgWrap = styled.div`
 export const Img = styled.img`
     width: 100%;
     margin: 0 0 10px 0;
+    background-color: #fbc433;
     padding-right: 0;
     border-radius: 16px;
     border: 1px solid #fbc433;
