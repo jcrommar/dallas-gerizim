529fb92acbc2e0c69c23a66adc1e44af1e1068e1
jcrommar <jcromualdo24@icloud.com>
2021-01-29 13:49:16 -0600

    changed to event section

--- src/components/EventSection/index.js
+++ src/components/EventSection/index.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import {
     ServicesContainer,
+    ServicesContainer2,
     ServicesH1,
-    ServicesCard,
-    ServicesH2,
+    NavLogoImg,
+    ServicesH2
 } from './eventElement';
 
 const Services = () => {
     return (
         <>
-          <ServicesContainer id="evento">
-            <ServicesH1>7º Congreso</ServicesH1>
-            <ServicesH1>De La Palabra</ServicesH1>
-            <ServicesCard>
-                <iframe width="350" height="315" title="streaming" src="https://www.youtube.com/embed/mYhbo-GGOt8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <ServicesH2>Apostol Raul Martinez</ServicesH2>
-            </ServicesCard>
-           {/*} <NavLogoImg src="images/live_now_gif.gif"></NavLogoImg> {*/}
+            <ServicesContainer2 id="evento">
+                <ServicesH1>
+                    7º Congreso
+                </ServicesH1>
+                <ServicesH1>
+                    De La Palabra
+                </ServicesH1>
+                <ServicesH2>
+                    Enero 29-31, 2021
+                </ServicesH2>
+                           <NavLogoImg src="images/youtube_giphy.gif"></NavLogoImg>
+            </ServicesContainer2>
+          <ServicesContainer className="video"
+                    id="evento"
+                    style={{
+                        position: "relative",
+                        display: "flex",
+                        paddingBottom: "56.25%" /* 16:9 */,
+                        paddingTop: 25,
+                        height: 0
+                    }}>
+                        <iframe
+                        title="streaming"
+                        style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%"
+                        }}
+                        src={`https://www.youtube.com/embed/mYhbo-GGOt8`}
+                        frameBorder="0"
+                    />
+            {/*} <NavLogoImg src="images/live_now_gif.gif"></NavLogoImg> {*/}
           </ServicesContainer>  
         </>
     )
