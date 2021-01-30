88ad6c9286e6d3e9fd807fdf8a70db6fb11dd59d
jcrommar <jcromualdo24@icloud.com>
2021-01-29 19:39:14 -0600

    update to live video

--- src/components/EventSection/index.js
+++ src/components/EventSection/index.js
@@ -40,7 +40,7 @@ const Services = () => {
                         width: "100%",
                         height: "100%"
                         }}
-                        src={`https://www.youtube.com/embed/ASzQaJP5aP0`}
+                        src={`https://www.youtube.com/embed/Zv-Ph2AhePI`}
                         frameBorder="0"
                     />
             {/*} <NavLogoImg src="images/live_now_gif.gif"></NavLogoImg> {*/}
