c140e1896badc5cce95c01e8a43276932db267d7
jcrommar <jcromualdo24@icloud.com>
2021-01-31 09:04:03 -0600

    edited youtube id for event

--- src/components/EventSection/index.js
+++ src/components/EventSection/index.js
@@ -52,7 +52,7 @@ const Services = () => {
                         width: "100%",
                         height: "100%"
                         }}
-                        src={`https://www.youtube.com/embed/0YURr0GhWbU`}
+                        src={`https://www.youtube.com/embed/3QmtvSHMtMc`}
                         frameBorder="0"
                     />
             {/*} <NavLogoImg src="images/live_now_gif.gif"></NavLogoImg> {*/}
