06795009379ca65691c3c4342738c99a0cfc1b15
jcrommar <jcromualdo24@icloud.com>
2021-02-11 20:21:23 -0600

    changed page name

---
+++ src/pages/youtube.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EventSection from '../components/EventSection';
+
+
+const SigninPage = () => {
+    return (
+        <div>
+            <EventSection />
+        </div>
+    )
+}
+
+export default SigninPage;
