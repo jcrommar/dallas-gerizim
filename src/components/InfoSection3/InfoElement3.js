fec2d350c943c79ed1b7393c659f35feaff6f63c
jcrommar <jcromualdo24@icloud.com>
2022-02-04 00:42:20 -0600

    made changes to the event section and map address section of the website to add new address

--- src/components/InfoSection3/InfoElement3.js
+++ src/components/InfoSection3/InfoElement3.js
@@ -70,7 +70,7 @@ export const TopLine = styled.p`
 
 export const Heading = styled.h1`
     margin-bottom: 24px;
-    font-size: 48px;
+    font-size: 35px;
     line-height: 1.1;
     font-weight: 600;
     color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#213159')};
--- src/components/InfoSection3/index.js
+++ src/components/InfoSection3/index.js
@@ -41,7 +41,7 @@ const InfoSection = ({
                                 <Heading lightText={lightText}>{headline}</Heading>
                                 <Subtitle darkText={darkText}>{description}</Subtitle>
                                 <BtnWrap>
-                                    <ButtonLink to="//google.com/maps/place/1499+Regal+Row,+Dallas,+TX+75247/@32.8348657,-96.8805066,17z/data=!4m5!3m4!1s0x864e9cf80c2af5f9:0xf216c9fd6e58612c!8m2!3d32.8348843!4d-96.8782809" target="_blank" aria-label="Direccion"
+                                    <ButtonLink to="//google.com/maps/place/1200+West+Shore+Dr,+Richardson,+TX+75080/@32.9666657,-96.7496225,17z/data=!3m1!4b1!4m5!3m4!1s0x864c1f5fa4e8042d:0xced14266b4f95d38!8m2!3d32.9666657!4d-96.7474338" target="_blank" aria-label="Direccion"
                                     smooth={true}
                                     duration={500}
                                     spy={true}
@@ -57,7 +57,10 @@ const InfoSection = ({
                         </Column1>
                         <Column2>
                             <ImgWrap>
-                            <iframe title="gerizim_map" width="300" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3352.391504112216!2d-96.88046958481648!3d32.83488428095454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e9cf80c2af5f9%3A0xf216c9fd6e58612c!2s1499%20Regal%20Row%2C%20Dallas%2C%20TX%2075247!5e0!3m2!1sen!2sus!4v1611537328685!5m2!1sen!2sus"></iframe><script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=6220d179aa9341408b0bfcaadc0e91c62b9226d8'></script>
+                            <iframe title="gerizim_map" width="300" height="450" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3347.4068741660562!2d-96.74962248481313!3d32.966665680915135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c1f5fa4e8042d%3A0xced14266b4f95d38!2s1200%20West%20Shore%20Dr%2C%20Richardson%2C%20TX%2075080!5e0!3m2!1sen!2sus!4v1643956629961!5m2!1sen!2sus">
+                                </iframe>
+                                <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=6220d179aa9341408b0bfcaadc0e91c62b9226d8'>
+                                </script>
                             </ImgWrap>
                         </Column2>
                     </InfoRow>
